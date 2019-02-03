c3944881f0ec2d4ad3729634ec344083bd9d5bc0
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-02-02 16:18:51 -0800

    chore(npm): release
    BREAKING CHANGE: Entirely new package

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-	"package.json": [
-		"sort-package-json",
-		"git add"
+	'package.json': [
+		'sort-package-json',
+		'git add',
 	],
-	"*.js": [
-		"eslint --report-unused-disable-directives --fix",
-		"git add"
+	'*.js': [
+		'eslint --report-unused-disable-directives --fix',
+		'git add',
 	],
-	"{*.css,*.scss}": [
-		"stylelint --fix",
-		"git add"
-	]
+	'{*.css,*.scss}': [
+		'stylelint --fix',
+		'git add',
+	],
 };
